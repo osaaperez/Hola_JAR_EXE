59b9fa4a02afdcbe0cd50e367bd7d8c85142544f
Óscar Saá Pérez <osaaperez@danielcastelao.org>
2020-12-17 16:04:33 +0100

    Este es el JAR y EXE del hola mundo!.

---
+++ src/com/ventana/Main.java
@@ -0,0 +1,10 @@
+package com.ventana;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola mundo");
+    }
+}
